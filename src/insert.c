aafa18f297ce6a66439858a54757972e625811a4
savalet <savinien.petitjean@gmail.com>
2025-01-08 20:05:12 +0100

    fix: duplicate value when insert

--- src/insert.c
+++ src/insert.c
@@ -22,12 +22,12 @@ int ht_insert(hashtable_t *ht, char *key, char *value)
         return -1;
     hash_i = hash_vl % hash_len;
     if (ht[hash_i].data == NULL)
-        ht[hash_i].data = e_create_linked_list(value, hash_vl);
-    else if (e_add_node(&ht[hash_i].data, value, hash_vl) == RETURN_FAILURE)
+        ht[hash_i].data = e_create_linked_list(e_strdup(value), hash_vl);
+    else if (e_add_node(&ht[hash_i].data, e_strdup(value), hash_vl) ==
+        RETURN_FAILURE)
         return FAILURE_MSG("Error when inserting data in node !");
     if (ht[hash_i].data == NULL)
         return FAILURE_MSG("Cannot create linked list to insert !");
-    MY_DEBUG("Hash len [%lu] hash value [%d] hash index [%lu]\n", hash_len,
-        hash_vl, hash_i);
+    MY_DEBUG("Inst hash_vl [%d] hash_i [%lu]\n", hash_len, hash_vl, hash_i);
     return hash_vl;
 }
