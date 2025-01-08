faaf48bbbf3b2130825cdfa6c9f7870c56e21de3
Alice Duvillier <alice.duvillier@epitech.eu>
2025-01-08 23:18:07 +0100

    add: delete / fix: for of dump

---
+++ src/delete.c
@@ -0,0 +1,66 @@
+/*
+** EPITECH PROJECT, 2025
+** secured
+** File description:
+** delete
+*/
+
+#include "eyes.h"
+#include "secured.h"
+
+static
+int delete_node(node_t **head, int hash_vl)
+{
+    node_t *tmp = *head;
+    node_t *prev = *head;
+
+    if (*head == NULL)
+        return RETURN_FAILURE;
+    if (tmp->data != NULL && tmp->hash == hash_vl) {
+        *head = tmp->next;
+        free(tmp->data);
+        free(tmp);
+        return RETURN_SUCCESS;
+    }
+    for (; tmp->next != NULL && tmp->hash != hash_vl; tmp = tmp->next) {
+        if (tmp->next == NULL && tmp->hash == hash_vl) {
+            prev->next = NULL;
+            free(tmp->data);
+            free(tmp);
+            return RETURN_SUCCESS;
+        }
+        prev = tmp;
+    }
+    if (tmp->next == NULL && tmp->hash != hash_vl) {
+        return RETURN_FAILURE;
+    }
+    prev->next = tmp->next;
+    free(tmp->data);
+    free(tmp);
+    return RETURN_SUCCESS;
+}
+
+int ht_delete(hashtable_t *ht, char *key)
+{
+    int hash_vl;
+    size_t hash_i;
+    size_t hash_len;
+
+    MY_DEBUG_MSG("bonjour1.\n");
+    if (ht == NULL)
+        return RETURN_FAILURE;
+    MY_DEBUG_MSG("bonjour2.\n");
+    hash_vl = ht->fnct_hash(key, e_strlen(key));
+    hash_len = ht->len;
+    if (!hash_len)
+        return RETURN_FAILURE;
+    MY_DEBUG_MSG("bonjour3.\n");
+    hash_i = hash_vl % hash_len;
+    if (ht[hash_i].data == NULL)
+        return RETURN_FAILURE;
+    MY_DEBUG_MSG("bonjour4.\n");
+    if (delete_node(&ht[hash_i].data, hash_vl) == RETURN_FAILURE)
+        return RETURN_FAILURE;
+    MY_DEBUG_MSG("bonjour5.\n");
+    return RETURN_SUCCESS;
+}
