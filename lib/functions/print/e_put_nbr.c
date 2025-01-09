bbe9d3415687be5a10d08804a7de921d7a20c62e
savalet <savinien.petitjean@gmail.com>
2025-01-09 15:12:14 +0100

    fix: e_put_nbr

--- lib/functions/print/e_put_nbr.c
+++ lib/functions/print/e_put_nbr.c
@@ -13,7 +13,23 @@ void put_overflow(void)
     e_putstr("-2147483648");
 }
 
-static
+int e_put_l_nbr(long int nb)
+{
+    if (nb == -2147483648)
+        nb++;
+    if (nb < 10 && nb >= 0) {
+        e_putchar(nb + 48);
+    } else if (nb < 0) {
+        e_putchar('-');
+        nb *= -1;
+        e_put_l_nbr(nb);
+    } else {
+        e_put_l_nbr(nb / 10);
+        e_put_l_nbr(nb % 10);
+    }
+    return 0;
+}
+
 int e_put_h_nbr(int nb)
 {
     short int n = (short int) nb;
@@ -31,7 +47,6 @@ int e_put_h_nbr(int nb)
     return 0;
 }
 
-static
 int e_put_ll_nbr(long long int nb)
 {
     if (nb < 10 && nb >= 0) {
@@ -43,24 +58,6 @@ int e_put_ll_nbr(long long int nb)
     } else {
         e_put_ll_nbr(nb / 10);
         e_put_ll_nbr(nb % 10);
-    }
-    return 0;
-}
-
-static
-int e_put_l_nbr(long int nb)
-{
-    if (nb == -2147483648)
-        nb++;
-    if (nb < 10 && nb >= 0) {
-        e_putchar(nb + 48);
-    } else if (nb < 0) {
-        e_putchar('-');
-        nb *= -1;
-        e_put_l_nbr(nb);
-    } else {
-        e_put_l_nbr(nb / 10);
-        e_put_l_nbr(nb % 10);
     }
     return 0;
 }
