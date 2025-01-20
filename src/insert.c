1750cd10211d9288b82f053eba824198a469a4cc
savalet <savinien.petitjean@gmail.com>
2025-01-20 14:49:25 +0100

    fix: colision with same key when insert

--- src/insert.c
+++ src/insert.c
@@ -8,6 +8,22 @@
 #include "eyes.h"
 #include "secured.h"
 
+static
+int update_value(node_t *node, int hash_vl, char *value)
+{
+    char *tmp_value;
+
+    for (; node->hash != hash_vl && node->next != NULL; node = node->next);
+    if (node->hash != hash_vl)
+        return RETURN_FAILURE;
+    tmp_value = node->data;
+    node->data = e_strdup(value);
+    if (node->data == NULL)
+        return RETURN_FAILURE;
+    free(tmp_value);
+    return RETURN_SUCCESS;
+}
+
 int ht_insert(hashtable_t *ht, char *key, char *value)
 {
     int hash_vl;
@@ -18,9 +34,9 @@ int ht_insert(hashtable_t *ht, char *key, char *value)
         return RETURN_FAILURE;
     hash_vl = ht->fnct_hash(key, e_strlen(key));
     hash_len = ht->len;
-    if (!hash_len)
-        return RETURN_FAILURE;
     hash_i = hash_vl % hash_len;
+    if (ht_search(ht, key) != NULL)
+        return update_value(ht[hash_i].data, hash_vl, value);
     if (ht[hash_i].data == NULL)
         ht[hash_i].data = e_create_linked_list(e_strdup(value), hash_vl);
     else if (e_add_node(&ht[hash_i].data, e_strdup(value), hash_vl) ==
