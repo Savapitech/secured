4799820251f007bcc379f3a809114b30999d78ea
savalet <savinien.petitjean@gmail.com>
2025-01-21 15:51:23 +0100

    upd: revert the hash function change

--- src/hash.c
+++ src/hash.c
@@ -7,20 +7,20 @@
 
 #include "secured.h"
 
-static
-const uint64_t FVN_OFFSET_BASIS = 0xcbf29ce484222325;
-
-static
-const uint64_t FVN_PRIME = 0x100000001b3;
-
 int hash(char *key, int len)
 {
-    uint64_t k = FVN_OFFSET_BASIS;
+    unsigned int hash = 0;
+    int i = 0;
 
     if (key == NULL)
-        return -1;
-    (void)len;
-    for (; *key != '\0'; key++)
-        k = (k * FVN_PRIME) ^ (uint64_t)*key;
-    return (int)k;
+        return RETURN_FAILURE;
+    for (; i < len; i++) {
+        hash += (unsigned char)key[i];
+        hash += (hash << 10);
+        hash ^= (hash >> 6);
+    }
+    hash += (hash << 3);
+    hash ^= (hash >> 11);
+    hash += (hash << 15);
+    return hash;
 }
